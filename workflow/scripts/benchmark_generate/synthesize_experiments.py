b2d00717411c0edc5825566546cd030539935b94
cividis <brandonlukas@gmail.com>
2025-03-23 07:53:04 -0500

    Update Snakefile to include additional benchmark metrics; refactor experiment synthesis for single bed file handling

--- workflow/scripts/benchmark_generate/synthesize_experiments.py
+++ workflow/scripts/benchmark_generate/synthesize_experiments.py
@@ -41,8 +41,18 @@ def synthesize_experiments(
 
     bed_files = [data_dir + x + ".bed" for x in ranked_list.index]
     weights = ranked_list["score"].tolist()
-    result = generate.synthesize_experiments(bed_files, weights)
+    result = generate_result(bed_files, weights)
     result.to_parquet(output_file)
+
+
+def generate_result(bed_files, weights):
+    if len(bed_files) == 1:
+        # "Hack" to make the function work with only one bed file
+        bed_files = [bed_files[0], bed_files[0]]
+        df = generate.synthesize_experiments(bed_files, weights).drop("file_1", axis=1)
+    else:
+        df = generate.synthesize_experiments(bed_files, weights)
+    return df
 
 
 synthesize_experiments(
